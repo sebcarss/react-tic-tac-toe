3d74f3c5a6c79287b43b3072d2fe1af077dad065
Seb Carss <seb.carss@gmail.com>
2020-04-28 12:51:21 +0100

    Add highlight for winning line

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import "./index.css";
 // This is a function component, which is the same as creating a class as shown
 // below but is less verbose.
 function Square(props) {
+  console.log("Square() -> " + JSON.stringify(props));
   return (
-    <button className="square" onClick={props.onClick}>
+    <button className={props.className} onClick={props.onClick}>
       {props.value}
     </button>
   );
@@ -26,16 +27,33 @@ function Square(props) {
 // }
 
 class Board extends React.Component {
+  getSquareClassName(i) {
+    let className = null;
+    const winningLine = this.props.winningLine;
+
+    if (winningLine.includes(i)) {
+      className = "winning-square";
+    } else {
+      className = "square";
+    }
+
+    console.log("Board.getSquareClassName() -> " + className);
+    return className;
+  }
+
   renderSquare(i) {
+    console.log("Board.renderSquare() -> " + i);
     return (
       <Square
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
+        className={this.getSquareClassName(i)}
       />
     );
   }
 
   render() {
+    console.log("Board.render() -> " + JSON.stringify(this.props));
     return (
       <div>
         <div className="board-row">
@@ -61,6 +79,7 @@ class Board extends React.Component {
 class Game extends React.Component {
   constructor() {
     super();
+    console.log("Game.constructor() -> " + JSON.stringify(this.state));
     this.state = {
       history: [
         {
@@ -69,11 +88,17 @@ class Game extends React.Component {
       ],
       xIsNext: true,
       stepNumber: 0,
+      winner: undefined,
     };
   }
 
   handleClick(i) {
-    console.log("Square clicked: " + i);
+    console.log(
+      "Game.handleClick() -> " +
+        JSON.stringify(this.state) +
+        " & square clicked = " +
+        i
+    );
 
     // In React state needs to be treated as immutable, so to update the array we need
     // to create a new array and set the state to be this - we cannot alter the original
@@ -91,45 +116,49 @@ class Game extends React.Component {
     squares[i] = this.state.xIsNext ? "X" : "O";
 
     this.setState({
-        history: history.concat([{
-            squares: squares,
-        }]),
-        xIsNext: !this.state.xIsNext,
-        stepNumber: history.length,
+      history: history.concat([
+        {
+          squares: squares,
+        },
+      ]),
+      xIsNext: !this.state.xIsNext,
+      stepNumber: history.length,
     });
   }
 
   jumpTo(step) {
+    console.log("Game.jumpTo() -> " + JSON.stringify(this.state));
     this.setState({
-        stepNumber: step,
-        xIsNext: (step % 2) === 0,
+      stepNumber: step,
+      xIsNext: step % 2 === 0,
     });
   }
 
   render() {
+    console.log("Game.render() -> " + JSON.stringify(this.state));
     const history = this.state.history;
     const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
-    console.log("stepNumber: " + this.state.stepNumber);
-
+    // Setup the time travel
     const moves = history.map((step, move) => {
-        const desc = move ? 
-            'Go to move #' + move :
-            'Go to game start';
-        
-            return (
-                <li key={move}>
-                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
-                </li>
-            );
+      const desc = move ? "Go to move #" + move : "Go to game start";
+
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
     });
 
+    // Update status
     let status;
+    let winningLine = Array(3).fill(null);
     if (winner) {
       status = "Player " + winner + " wins!";
+      winningLine = calculateWinningLine(current.squares);
     } else if (this.state.stepNumber === 9) {
-        status = "Game drawn";
+      status = "Game drawn";
     } else {
       status = "Next player: " + (this.state.xIsNext ? "X" : "O");
     }
@@ -140,6 +169,7 @@ class Game extends React.Component {
           <Board
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
+            winningLine={winningLine}
           />
         </div>
         <div className="game-info">
@@ -151,7 +181,29 @@ class Game extends React.Component {
   }
 }
 
+function calculateWinningLine(squares) {
+  console.log("calculateWinningList() -> " + JSON.stringify(JSON));
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return [a, b, c];
+    }
+  }
+}
+
 function calculateWinner(squares) {
+  console.log("calculateWinner() -> " + JSON.stringify(squares));
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
